c10b09fcef8d65b41141d2611e388da00c2f3270
KSN <redawewrq12@gmail.com>
2025-06-08 20:37:17 +0300

    Delete common/service.sh

--- common/service.sh
+++
@@ -1,9 +0,0 @@
-#!/system/bin/sh
-
-stop xosupdater
-stop xosanalytics
-stop xoscloud
-
-echo "perfomance" > /sys/devices/system/cpu/cpu0/cpufreq/scaling_governor
-echo 90 > /sys/devices/system/cpu/cpufreq/interactive/go_hispeed_load
-echo 1200000 > /sys/devices/system/cpu/cpufreq/interactive/hispeed_freq
